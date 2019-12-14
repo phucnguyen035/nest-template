b3079b8905b3e8b046b39b455c65a9b4bee8e284
Phúc Nguyễn <nguyen.hphuc035@gmail.com>
2019-12-15 01:23:04 +0700

    ✨ add favicon

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
+import serveFavicon from 'serve-favicon';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(serveFavicon(join(__dirname, '../', 'favicon.ico')));
+
   await app.listen(3000);
 }
 bootstrap();
