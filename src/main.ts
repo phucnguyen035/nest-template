eac85d8bd0afd05fbe1c38a96c21dd57186125a5
Phúc Nguyễn <nguyen.hphuc035@gmail.com>
2019-12-15 02:01:58 +0700

    ✨ add cors and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { join } from 'path';
 import serveFavicon from 'serve-favicon';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.use(helmet.hidePoweredBy());
   app.use(serveFavicon(join(__dirname, '../', 'favicon.ico')));
 
   await app.listen(3000);
