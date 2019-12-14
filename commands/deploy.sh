f8e70251c4f340f99364d099ec79a2f226b2fe3e
Phúc Nguyễn <nguyen.hphuc035@gmail.com>
2019-12-15 02:51:56 +0700

    🐳 update deploy script

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -1,11 +1,20 @@
+name="api-prod"
+existing=$(docker container ls -q -f name=$name)
+
 echo 'Building project'
 npm run build
 
 echo 'Building docker image'
 docker build --build-arg NODE_ENV=production -t api $(dirname "pwd")
 
-echo 'Removing dangling images'
-docker image prune -f
+if [ $existing ]
+then
+  echo 'Stopping existing container'
+  docker container stop $existing
+fi
 
 echo 'Running image in background'
-docker container run --rm -p 3000:3000 -d api
+docker container run --rm --name $name -p 3000:3000 -d api
+
+echo 'Removing dangling images'
+docker image prune -f
