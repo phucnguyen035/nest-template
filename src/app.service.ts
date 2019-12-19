de90b3945edcf89b9286b44fc44554aa28640330
Phúc Nguyễn <nguyen.hphuc035@gmail.com>
2019-12-19 10:11:35 +0700

    ♻️ add hostname

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { hostname } from 'os';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return `Environment: ${
       process.env.NODE_ENV
-    }. It is currently: ${new Date().toLocaleString('vi-vn')}`;
+    }. Current node is: ${hostname()}. It is currently: ${new Date().toLocaleString(
+      'vi-vn',
+    )}`;
   }
 }
