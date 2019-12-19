0e361a8fbf8ccac4dfb081df5b9796a5728a6aa9
Phúc Nguyễn <nguyen.hphuc035@gmail.com>
2019-12-19 17:42:16 +0700

    ♻️ use registerAs from nestjs config
    add configuration namespace

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,13 +1,13 @@
-export default () => ({
-  database: {
-    // Defaults to postgres
-    type: process.env.DB_TYPE || 'postgres',
-    connection: {
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT, 10) || 5432,
-      user: process.env.DB_USERNAME || 'postgres',
-      database: process.env.DB_DATABASE || 'postgres',
-      password: process.env.DB_PASSWORD,
-    },
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  // Defaults to postgres
+  type: process.env.DB_TYPE || 'postgres',
+  connection: {
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    user: process.env.DB_USERNAME || 'postgres',
+    database: process.env.DB_DATABASE || 'postgres',
+    password: process.env.DB_PASSWORD,
   },
-});
+}));
