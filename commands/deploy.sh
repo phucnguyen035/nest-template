79b0185cf37fbb9de80fb9c350ac759815472dca
Phúc Nguyễn <nguyen.hphuc035@gmail.com>
2019-12-19 11:27:49 +0700

    🐳 use docker swarm for deployment

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -1,20 +1,26 @@
-name="api-prod"
-existing=$(docker container ls -q -f name=$name)
+# Change these variables for each project 
+NAME=api-prod # Service name
+IMAGE=api # Image used for building project
+PORT=3000 # Port used for exposing service to the public
+
+# Constant values, don't change if you don't need to
+TAG=$(git rev-parse --short HEAD) # Using latest git commit hash as tag
+EXISTING=$(docker service ls -q -f name=$NAME) # Check if service already exists
 
 echo 'Building project'
 npm run build
 
 echo 'Building docker image'
-docker build --build-arg NODE_ENV=production -t api $(dirname "pwd")
+docker build --build-arg NODE_ENV=production -t $IMAGE:$TAG $(dirname "pwd")
 
-if [ $existing ]
+if [ $EXISTING ]
 then
-  echo 'Stopping existing container'
-  docker container stop $existing
+  echo 'Updating existing service'
+  docker service update --force $EXISTING
+else
+  echo 'Running service for the first time'
+  docker service create --name $NAME -p $PORT:3000 --restart-condition=on-failure $IMAGE:$TAG
 fi
-
-echo 'Running image in background'
-docker container run --rm --name $name -p 3000:3000 -d api
 
 echo 'Removing dangling images'
 docker image prune -f
