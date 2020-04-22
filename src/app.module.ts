f6b960e11a279a5b3d5429f3b519470e7d3657fe
Phúc Nguyễn <nguyen.hphuc035@gmail.com>
2020-04-22 19:22:36 +0700

    🔥 remove database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { appConfig, databaseConfig } from './config';
-import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [appConfig, databaseConfig], isGlobal: true }),
-    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
