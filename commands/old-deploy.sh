048877b2fcc134a8088465c65bec82cb5b716e70
Phúc Nguyễn <nguyen.hphuc035@gmail.com>
2020-04-23 01:25:24 +0700

    ⚗ create new deploy script

--- commands/old-deploy.sh
+++ commands/old-deploy.sh
@@ -1,28 +1,30 @@
 # Change these variables for each project 
-NAME=api # Service name
+NAME=api-prod # Service name
+IMAGE=api # Image used for building project
 PORT=3000 # Port used for exposing service to the public
 
 # Constant values, don't change if you don't need to
+TAG=$(git rev-parse --short HEAD) # Using latest git commit hash as tag
 EXISTING=$(docker service ls -q -f name=$NAME) # Check if service already exists
 
-echo 'Pull image from registry'
-docker pull hoangphuc0305/test-api:latest
+echo 'Building docker image'
+docker build -t $IMAGE:$TAG .
 
 # Using docker swarm to deploy
-# Make sure swarm mode is active in your server (docker swarm init)
+# Make sure swarm mode is active in your server
 if [ $EXISTING ]
 then
   echo 'Updating existing service'
-  docker service update $EXISTING
+  docker service update --force $EXISTING
 else
   echo 'Running service for the first time'
   docker service create \
     --name $NAME \
     -p $PORT:3000 \
-    --restart-condition unless-stopped \
-    # --replicas-max-per-node 3 \
+    --restart-condition on-failure \
+    --replicas-max-per-node 3 \
     # --replicas 3 # Use this to set initial scale
-    hoangphuc0305/test-api:latest
+    $IMAGE:$TAG
 fi
 
 echo 'Removing dangling images'
