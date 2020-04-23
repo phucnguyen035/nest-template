666cf7707e8fbd27e3e1d7421db3033b5315622d
Phúc Nguyễn <nguyen.hphuc035@gmail.com>
2020-04-23 11:16:19 +0700

    💚 update deploy script to use docker run
    docker swarm does not work with docker-compose nginx letsencrypt companion

---
+++ commands/deploy-swarm.sh
@@ -0,0 +1,32 @@
+# WARNING: Does not work with nginx letsencrypt companion!! Create your own nginx if using this
+# Change these variables for each project
+NAME="${NAME:-api}" # Service name
+PORT="${PORT:-3000}" # Port used for exposing service to the public
+
+# Constant values, don't change if you don't need to
+EXISTING=$(docker service ls -q -f name=$NAME) # Check if service already exists
+
+echo 'Pulling image from registry'
+docker pull hoangphuc0305/test-api:latest
+
+# Using docker swarm to deploy
+# Make sure swarm mode is active in your server (docker swarm init)
+# Comment out VIRTUAL_HOST/PORT, LETSENCRYPT_HOST/EMAIL if not using nginx let's encrypt companion
+if [ $EXISTING ]
+then
+  echo 'Updating existing service'
+  docker service update $EXISTING
+else
+  echo 'Running service for the first time'
+  docker service create --name $NAME \
+    -p $PORT:3000 \
+    -e VIRTUAL_HOST=api.nguyenhphuc.com \
+    -e VIRTUAL_PORT=$PORT \
+    -e LETSENCRYPT_HOST=api.nguyenhphuc.com \
+    -e LETSENCRYPT_EMAIL=nguyen.hphuc035@gmail.com \
+    --network webproxy \
+    --restart-condition on-failure \
+    hoangphuc0305/test-api:latest
+    # --replicas-max-per-node 3 \
+    # --replicas 3 # Use this to set initial scale
+fi
--- commands/deploy.sh
+++ commands/deploy.sh
@@ -3,32 +3,26 @@ NAME="${NAME:-api}" # Service name
 PORT="${PORT:-3000}" # Port used for exposing service to the public
 
 # Constant values, don't change if you don't need to
-EXISTING=$(docker service ls -q -f name=$NAME) # Check if service already exists
-
-echo 'Pulling latest changes from git'
-git pull
+EXISTING=$(docker container ls -q -f name=$NAME) # Check if service already exists
 
 echo 'Pulling image from registry'
 docker pull hoangphuc0305/test-api:latest
 
-# Using docker swarm to deploy
 # Make sure swarm mode is active in your server (docker swarm init)
 # Comment out VIRTUAL_HOST/PORT, LETSENCRYPT_HOST/EMAIL if not using nginx let's encrypt companion
 if [ $EXISTING ]
 then
-  echo 'Updating existing service'
-  docker service update $EXISTING
-else
-  echo 'Running service for the first time'
-  docker service create --name $NAME \
-    -p $PORT:3000 \
-    -e VIRTUAL_HOST=api.nguyenhphuc.com \
-    -e VIRTUAL_PORT=$PORT \
-    -e LETSENCRYPT_HOST=api.nguyenhphuc.com \
-    -e LETSENCRYPT_EMAIL=nguyen.hphuc035@gmail.com \
-    --network webproxy \
-    --restart-condition on-failure \
-    hoangphuc0305/test-api:latest
-    # --replicas-max-per-node 3 \
-    # --replicas 3 # Use this to set initial scale
+  echo 'Updating existing container'
+  docker container stop $EXISTING
 fi
+
+echo 'Creating and running new container'
+docker container run -dit --name $NAME \
+  -p $PORT:3000 \
+  -e VIRTUAL_HOST=api.nguyenhphuc.com \
+  -e VIRTUAL_PORT=$PORT \
+  -e LETSENCRYPT_HOST=api.nguyenhphuc.com \
+  -e LETSENCRYPT_EMAIL=nguyen.hphuc035@gmail.com \
+  --network=webproxy \ 
+  --restart=unless-stopped \
+  hoangphuc0305/test-api:latest
