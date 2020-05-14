1d124c0a868de0268b31ef911689f43a407da82e
Phúc Nguyễn <nguyen.hphuc035@gmail.com>
2020-05-14 16:51:33 +0700

    ✨ add port using env

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,4 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   environment: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
 }));
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { join } from 'path';
@@ -6,10 +7,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const config = app.get(ConfigService);
+  const port = config.get<number>('app.port');
 
   app.use(helmet.hidePoweredBy());
   app.use(serveFavicon(join(__dirname, '../', 'favicon.ico')));
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
